b80056c35d7c08916d860ba11295475bed30a172
joao victor santos rodrigues <imashinzo10@gmail.com>
2023-03-29 11:31:03 -0300

    Refatoramento index.ts archtypes

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,3 +1,13 @@
 import Archetype from './Archetype';
+import Warrior from './Warrior';
+import Necromancer from './Necromancer';
+import Ranger from './Ranger';
+import Mage from './Mage';
 
 export default Archetype;
+export {
+  Warrior,
+  Necromancer,
+  Ranger,
+  Mage,
+};
